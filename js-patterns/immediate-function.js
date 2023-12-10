42a85982beadf2a6862ce6e62d448fac5bd60a4c
HossamTuwano <hossamtuwano@gmail.com>
2023-12-10 13:09:24 +0300

    immediate function with parameters

--- js-patterns/immediate-function.js
+++ js-patterns/immediate-function.js
@@ -3,5 +3,11 @@
 	today = new Date();
     msg = 'Today is ' + days[today.getDay()] + ', ' + today.getDate();
     console.log(msg);
-}()); //
+}()); // Today is
+
+// Adding parameters
+
+(function (who, when) {
+    console.log('I met ' + who + ' on ' + when);
+}('Sirius Back', new Date()))
 
