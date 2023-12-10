84c4a50a7b0c077e4f7d7695a43c40709d00c140
HossamTuwano <hossamtuwano@gmail.com>
2023-12-10 13:51:31 +0300

    immediate function inside an object

--- js-patterns/immediate-function.js
+++ js-patterns/immediate-function.js
@@ -11,3 +11,49 @@
     console.log('I met ' + who + ' on ' + when);
 }('Sirius Back', new Date()))
 
+// passing global so that
+// it's accessinble inside the fn
+// without having to pass window
+
+//(function (global) {
+    // access the global object via 'global'
+//}(this));
+
+
+// returned values from immediate function
+
+var result = (function () {
+    return 2 + 2;
+}());
+
+result;
+
+// storing returned value
+// in a function closure
+
+var getResult = (function () {
+    var res = 2 + 2;
+    return function () {
+	return res;
+    }
+}())
+
+getResult();
+
+// immediate function inside an object
+
+var o = {
+    message: (function () {
+	var who = 'me',
+	    what = 'call';
+	return what  + ' ' + who;
+    }()),
+    getMsg: function () {
+	return this.message;
+    }
+};
+
+// usage
+
+o.getMsg();
+o.message;
