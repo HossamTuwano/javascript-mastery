e55632e0c338658e9e301d9a9a2216adfb50eb1e
HossamTuwano <hossamtuwano@gmail.com>
2024-01-25 09:05:41 +0300

    returned values to local var

--- js-patterns/namespace-pattern.js
+++ js-patterns/namespace-pattern.js
@@ -6,19 +6,45 @@
 // avoid naming collision
 
 // global object
-var MYAPP = {};
+var MYAPP = {}
 
 // constructor
-MYAPP.Parent = function () {};
-MYAPP.Child = function () {};
+MYAPP.Parent = function () {}
+MYAPP.Child = function () {}
 
 // a variable
-MYAPP.some_var = 1;
+MYAPP.some_var = 1
 
 // an object container
-MYAPP.modules = {};
+MYAPP.modules = {}
 
 // nested module
-MYAPP.modules.module1 = {};
-MYAPP.modules.module1.data = { a: 1, b: 2 };
-MYAPP.modules.module2 = {};
+MYAPP.modules.module1 = {}
+MYAPP.modules.module1.data = { a: 1, b: 2 }
+MYAPP.modules.module2 = {}
+
+//before adding a namespace its good to check if it exists
+const MYAPP = MYAPP || {}
+
+MYAPP.namespace = function (ns_string) {
+  var parts = ns_string.split('.')
+  parent = MYAPP
+  i
+  // strip redundant leading global
+  if (parts[0] === 'MYAPP') {
+    parts == parts.slice(1)
+  }
+
+  for (i = 0; i < parts.length; i += 1) {
+    // create a property if it doesn't exist
+    if (typeof parent[parts[i]] === 'undefined') {
+      parent[parts[i]] = {}
+    }
+    parent = parent[parts[i]]
+  }
+  return parent
+}
+
+//assign returned values to local var
+var module2 = MYAPP.namespace('MYAPP.modules.modules2')
+module2 === MYAPP.modules.module2; // true
