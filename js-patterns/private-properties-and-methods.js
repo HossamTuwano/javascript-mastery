bc7f4a1bd80b33b5086c3cd11c0f201e35e592a7
HossamTuwano <hossamtuwano@gmail.com>
2024-01-30 08:33:01 +0300

    privacy failures

--- js-patterns/private-properties-and-methods.js
+++ js-patterns/private-properties-and-methods.js
@@ -21,10 +21,35 @@ console.log(toy.name) // undefined
 // public method has access to `name`
 console.log(toy.getName()) // "iPod"
 
-
 // Privilaged members
 // there is no syntax to express privilaged members rather than
 // it is a term given to the public method getName
-// because it has a special accest to the private member name 
+// because it has a special accest to the private member name
+
+// privacy failures
+// when accessing the privileged method that return a private member
+// when that member is and object or an array the consumer can choose
+// to modify the properties of the private member
+
+function Gadget2() {
+  // private member
+  var specs = {
+    screen_width: 320,
+    screen_height: 480,
+    color: 'white',
+  }
+  // public function
+  this.getSpecs = function () {
+    return specs
+  }
+}
+
+var toy = new Gadget(),
+  specs = toy.getSpecs()
+
+specs.color = 'black'
+specs.price = 'free'
+
+console.dir(toy.getSpecs())
 
 
