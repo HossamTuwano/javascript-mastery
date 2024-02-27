56f2d17979c4936e52972ebb37b80cdde5f38909
HossamTuwano <hossamtuwano@gmail.com>
2024-02-27 09:40:39 +0300

    static method

--- js-patterns/static-members.js
+++ js-patterns/static-members.js
@@ -1,24 +1,49 @@
-var Gadget = function () {
-    
-}
+var Gadget = function () {}
 
 Gadget.isShiny = function () {
-    return 'You bet'
+  return 'You bet'
 }
 
 Gadget.prototype.setPrice = function (price) {
-    this.price = price;
+  this.price = price
 }
 
 //  calling a staic method
-Gadget.isShiny(); // 'you bet'
+Gadget.isShiny() // 'you bet'
 
 // createing an instance and calling method
-var iphone = new Gadget();
-iphone.setPrice(500);
+var iphone = new Gadget()
+iphone.setPrice(500)
 
-typeof Gadget.setPrice; // 'undefined'
-typeof iphone.isShiny; // 'undefined'
+typeof Gadget.setPrice // 'undefined'
+typeof iphone.isShiny // 'undefined'
 
-Gadget.prototype.isShiny = Gadget.isShiny;
+Gadget.prototype.isShiny = Gadget.isShiny
 iphone.isShiny() //'you bet'
+
+// constructor
+var Gadget = function (price) {
+  this.price = price
+}
+
+// a static method
+Gadget.isShiny = function () {
+  // this always works
+  var msg = 'you bet'
+  if (this instanceof Gadget) {
+    // this works if only called statically
+    msg += ', it costs $' + this.price + '!'
+  }
+
+  return msg
+}
+
+// a normal method added to the prototype
+Gadget.prototype.isShiny = function () {
+  return Gadget.isShiny.call(this)
+}
+
+Gadget.isShiny() // "you bet"
+
+var a = new Gadget('499.99');
+a.isShiny(); // "you bet, it costs $499.99!"
--- jskata/promise-all.js
+++ jskata/promise-all.js
@@ -7,4 +7,3 @@ const promise3 = new Promise((resolve, reject) => {
 Promise.all([promise1, promise2, promise3]).then((values) => {
   console.log(values)
 })
-// Exp
