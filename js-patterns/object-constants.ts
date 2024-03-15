b5d2f47352a2bb8faed9a0b1983456dcb51680c5
HossamTuwano <hossamtuwano@gmail.com>
2024-03-15 15:43:42 +0300

    object-constants

---
+++ js-patterns/object-constants.ts
@@ -0,0 +1,13 @@
+// there are no constants in js
+// for convention sake it is advised to use uppercase
+// when naming variable that won't change
+
+// constructor
+var Widget = function () {
+  // implementation
+  var MAX_WIDTH = function () {}
+  var MAX_WIDTH = function () {}
+}
+
+// constants
+;(Widget.MAX_WIDTH = 320), (Widget.MAX_HEIGHT = 480)
