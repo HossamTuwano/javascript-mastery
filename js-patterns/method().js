184bd09a148dd238f7c11030259f8e41f1eca4f8
HossamTuwano <hossamtuwano@gmail.com>
2024-03-26 21:19:01 +0300

    -lesson: method() method

--- js-patterns/method().js
+++ js-patterns/method().js
@@ -1,10 +1,20 @@
+// constructor is chained to methods
+// methods take two parameters
+// method name and the implementation of the method
+
 var Person = function (name) {
   this.name = name
 }
+  .method('getName', function () {
+    return this.name
+  })
+  .method('setName', function (name) {
+    this.name = name
+    return this
+  })
 
-method('getName', function () {
-  return this.name
-}).method('setName', function (name) {
-  this.name = name
-  return this
-})
+// here is how you can use Person()
+// to create and use a new object
+
+var a = new Person('Adam')
+a.getName() // 'Adm'
